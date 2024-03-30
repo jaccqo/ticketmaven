5dc755621e6b79738c9a0ded49e893b76afdca72
Jack Jack <jack@Jacks-MacBook-Pro.local>
2024-03-30 22:22:05 +0300

    maven

--- ticketmaven/ticketmaven/settings.py
+++ ticketmaven/ticketmaven/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--7n@z#ak9f$kadfyn4&@jhq7g0d3$c4z=eh19cgqi+u(1(_s(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost","45.79.209.122","ticketmaven.com"]
 
